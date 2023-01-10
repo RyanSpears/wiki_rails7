be6e39ae62be4365f3ffc397c4cd15d101acaac1
Ryan Spears <sperj001@hotmail.com>
2023-01-10 17:12:42 +1030

    Added authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+  def login
+    if params["username"]
+      user = User.find_by_username(params["username"])
+
+      if !user.nil?
+        p 'Found user ' + user.username
+
+        @auth = user.authenticate(params["password"])
+
+        if @auth
+          p "You've signed in."
+          p @auth
+          return
+        end
+      end
+
+      p "Login failed."
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      p @user.errors.count
+      redirect_to @user, alert: "User created successfully"
+    else
+      redirect_to new_user_path, aler: "Error creating user"
+    end
+  end
+
+  def user_params
+    params.required(:user).permit(:username, :password, :salt, :encrypted_password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'about', to: redirect('/welcome/about')
+  get "users/new"
+  get "users/create"
+  get "users/login"
+  post "users/login"
+  get "welcome/index"
+  get "welcome/about"
+  get "about", to: redirect("/welcome/about")
 
   resources :wiki_posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "welcome#index"
+
+  resources :users
 end
